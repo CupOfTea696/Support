61e474a25d291f159f468070a4f2575060d37c51
CupOfTea696 <cupoftea696@gmail.com>
2016-01-06 12:27:37 +0000

    object_merge helper

--- src/helpers.php
+++ src/helpers.php
@@ -7,6 +7,13 @@ if (! function_exists('swap')) {
     }
 }
 
+if (! function_exists('object_merge')) {
+    function object_merge($obj1, $obj2)
+    {
+        return (object) array_merge((array) $obj1, (array) $obj2);
+    }
+}
+
 if (! function_exists('error')) {
     function error($message)
     {
